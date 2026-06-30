5d64aa010ac87d4799cb8d3e39ea96e8cc6e1bd0
Vijay Kumar <vijaykumarkandagatla55@gmail.com>
2026-06-30 21:29:18 +0530

    Pipeline with params added.

--- src/model_building.py
+++ src/model_building.py
@@ -28,22 +28,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-# def load_params(params_path: str) -> dict:
-#     """Load parameters from a YAML file."""
-#     try:
-#         with open(params_path, 'r') as file:
-#             params = yaml.safe_load(file)
-#         logger.debug('Parameters retrieved from %s', params_path)
-#         return params
-#     except FileNotFoundError:
-#         logger.error('File not found: %s', params_path)
-#         raise
-#     except yaml.YAMLError as e:
-#         logger.error('YAML error: %s', e)
-#         raise
-#     except Exception as e:
-#         logger.error('Unexpected error: %s', e)
-#         raise
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(file_path: str) -> pd.DataFrame:
@@ -81,8 +81,8 @@ def train_model(X_train: np.ndarray, y_train: np.ndarray, params: dict) -> Rando
             raise ValueError("The number of samples in X_train and y_train must be the same.")
         
         logger.debug('Initializing RandomForest model with parameters: %s', params)
-        # clf = RandomForestClassifier(n_estimators=params['n_estimators'], random_state=params['random_state'])
-        clf = RandomForestClassifier(n_estimators=25, random_state=2)
+        clf = RandomForestClassifier(n_estimators=params['n_estimators'], random_state=params['random_state'])
+        # clf = RandomForestClassifier(n_estimators=25, random_state=2)
         logger.debug('Model training started with %d samples', X_train.shape[0])
         clf.fit(X_train, y_train)
         logger.debug('Model training completed')
@@ -119,8 +119,7 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators':25,
-                  'random_state':2}
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
